25bf691d10b1f4cea2718a69e77c73585c2ac9fb
Andy <andysatie@gmail.com>
2021-12-16 09:58:01 -0300

    se agregó la lógica al controlador para crear productos

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -6,8 +6,24 @@ productoCtrl.borrarProducto = (req, res) => {
     res.send('alguien quiere borrar')
 };
 
-productoCtrl.nuevoProducto = (req, res) => {
-    res.send('alguien quiere agregar un producto')
+productoCtrl.nuevoProducto = async(req, res) => {
+    try {
+        //console.log(req.body)
+        //validar
+        //crear un objeto para guardar en la BD
+        const productoNuevo = new Producto({
+            nombreProducto: req.body.nombreProducto,
+            precioProducto: req.body.precioProducto,
+            categoria: req.body.categoria
+        })
+        //console.log(productoNuevo)
+        //guardar en base de datos
+        await productoNuevo.save();
+        res.status(201).json({mensaje: 'Producto creado correctamente'});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({mensaje: 'Error al crear el producto'});
+    }
 };
 
 export default productoCtrl
